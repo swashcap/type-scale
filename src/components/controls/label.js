e05539d9a8e8a9e7882fa68dab4f31950226ab80
Cory Reed <swashcap@gmail.com>
2019-08-11 12:23:41 -0700

    refactor: allow custom `Label` element

--- src/components/controls/label.js
+++ src/components/controls/label.js
@@ -3,6 +3,6 @@ import { h } from 'preact'
 
 import styles from './label.css'
 
-export default ({ class: className, ...rest }) => (
-  <label class={`${styles.label} ${className}`} {...rest} />
+export default ({ class: className, element: Component = 'label', ...rest }) => (
+  <Component class={`${styles.label} ${className}`} {...rest} />
 )
