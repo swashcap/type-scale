98573d68843a3b2dedf8ea977fdd012d6417989a
Cory Reed <swashcap@gmail.com>
2019-08-11 11:57:44 -0700

    refactor: change display size position

--- src/components/heading/index.js
+++ src/components/heading/index.js
@@ -3,22 +3,35 @@ import { h } from 'preact'
 
 import style from './style.css'
 
-export default ({ children, class: className, element: Component = 'p', size, ...rest }) => {
+export default ({
+  children,
+  class: className,
+  displayUnit = 'px',
+  element: Component = 'p',
+  size,
+  ...rest
+}) => {
   const fontSize = Math.round(size * 100) / 100
   const emSize = Math.round(size * 100) / 1600
+
+  const displaySize = displayUnit === 'px'
+    ? (
+
+      <code class={style['heading-code']}>
+        {fontSize % 1 ? fontSize.toFixed(2) : fontSize}<abbr title='pixels'>px</abbr>
+      </code>
+    ) : (
+      <code class={style['heading-code']}>
+        {emSize % 1 ? emSize.toFixed(2) : emSize}em
+      </code>
+    )
 
   return (
     <div class={`${style.heading} ${className}`} {...rest}>
       <Component class={style['heading-component']} style={{ fontSize: `${fontSize}px` }}>
         {children}
       </Component>
-      <div>
-        <code class={style['heading-code']}>
-          {fontSize % 1 ? fontSize.toFixed(2) : fontSize}px
-          {' / '}
-          {emSize % 1 ? emSize.toFixed(2) : emSize}em
-        </code>
-      </div>
+      {displaySize}
     </div>
   )
 }
